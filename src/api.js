ddac5df1ec4b423e2f9b5fd1b2a6808022eb884b
W3bwizard <w3bwiz4rd@gmail.com>
2021-06-20 00:47:07 +0300

    get coin price in BTC

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 const API_KEY = '74eea2552d64bb162020979867150f7c48bd83b30c10b95afae842e42ae12384'
-const tickersHandlers = new Map()
+const tickersHandlersUSD = new Map()
+const tickersHandlersBTC = new Map()
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
 
+// Если добавить тикер BTC и потом удалить, то удаляется фоновая подписка на BTC для курса обмена
 
 socket.onopen = function() {
     sendToWS(
@@ -15,11 +17,11 @@ socket.onopen = function() {
 socket.onmessage = function(event) {
     let message = JSON.parse(event.data)
     
-    
     if (message.TYPE === '5' 
-        && tickersHandlers.has(message.FROMSYMBOL) 
-        && message.TOSYMBOL === 'USD') {
-        const handler = tickersHandlers.get(message.FROMSYMBOL)
+        && tickersHandlersUSD.has(message.FROMSYMBOL) 
+        && message.TOSYMBOL === 'USD'
+        && message.FLAGS != '4') {
+        const handler = tickersHandlersUSD.get(message.FROMSYMBOL)
         handler(message.FROMSYMBOL, message.PRICE)
     }
     if (message.TYPE === '5' 
@@ -28,20 +30,15 @@ socket.onmessage = function(event) {
     }
     if (message.TYPE === '500' 
         && message.MESSAGE === 'INVALID_SUB') {
-        console.log(message.PARAMETER)
-        subscribeToTickerOnWSToBTC(getSymbolFromParametr(message.PARAMETER))
+        subscribeToTickerByBTC(message.PARAMETER, socket.TickerListUpdater)
     }
     if (message.TYPE === '5' 
+        && tickersHandlersBTC.has(message.FROMSYMBOL)
         && message.TOSYMBOL === 'BTC') {
-        console.log(message.FROMSYMBOL, message.PRICE)
+        const handler = tickersHandlersBTC.get(message.FROMSYMBOL)
+        handler(message.FROMSYMBOL, message.PRICE)
+        // console.log(message.FROMSYMBOL, message.PRICE)
     }
-}
-
-function getSymbolFromParametr(parameter) {
-    // let re = new RegExp('5~CCCAGG~([\\s\\S]*)~USD')
-    // let match = re.exec(parameter)
-    // return match[1]
-    return parameter.substring(parameter.lastIndexOf('CCCAGG~')+1,parameter.lastIndexOf('~USD'))
 }
 
 function sendToWS(msg){
@@ -62,10 +59,31 @@ function sendToWS(msg){
         
 }
 
-function subscribeToTickerOnWSToBTC(tickerName){
+function getCoinNameFromParameter(parameter) {
+    if (parameter.indexOf('5~CCCAGG~') != -1) {
+        const FROM_INDEX = parameter.indexOf('5~CCCAGG~') + '5~CCCAGG~'.length
+        const TO_INDEX = parameter.indexOf('~USD')
+        const COIN_NAME = parameter.slice(FROM_INDEX, TO_INDEX)
+        
+        return COIN_NAME
+    } else {
+        return null
+    }
+}
+
+function subscribeToTickerOnWSByBTC(tickerName){
     sendToWS(
         {
         action: 'SubAdd',
+        subs: [`5~CCCAGG~${tickerName}~BTC`]
+        }
+    )
+}
+
+function unsubscribeFromTickerOnWSByBTC(tickerName){
+    sendToWS(
+        {
+        action: 'SubRemove',
         subs: [`5~CCCAGG~${tickerName}~BTC`]
         }
     )
@@ -89,16 +107,32 @@ function unsubscribeFromTickerOnWS(tickerName){
     )
 }
 
+function subscribeToTickerByBTC(parameter, cb) {
+    const TICKER_NAME = getCoinNameFromParameter(parameter)
+    if (TICKER_NAME) {
+        tickersHandlersBTC.set(TICKER_NAME, cb)
+        subscribeToTickerOnWSByBTC(TICKER_NAME)
+    }
+}
+
 export function subscribeToTicker(tickerName, cb) {
-    tickersHandlers.set(tickerName, cb)
+    socket.TickerListUpdater = cb
+    tickersHandlersUSD.set(tickerName, cb)
     subscribeToTickerOnWS(tickerName)
 }
 
 export function unsubscribeFromTicker(tickerName) {
-    tickersHandlers.delete(tickerName)
-    unsubscribeFromTickerOnWS(tickerName)    
+    if (tickersHandlersUSD.has(tickerName)) {
+        tickersHandlersUSD.delete(tickerName)
+        unsubscribeFromTickerOnWS(tickerName)    
+    }
+    if (tickersHandlersBTC.has(tickerName)) {
+        tickersHandlersBTC.delete(tickerName)
+        unsubscribeFromTickerOnWSByBTC(tickerName)
+    }
 }
 
-window.tickers = tickersHandlers
+window.tickers = tickersHandlersBTC
+window.GetCoinName = getCoinNameFromParameter
 
 
